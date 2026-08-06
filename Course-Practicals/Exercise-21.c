db757b1a5a94cfa05f4d65e382027c603fa6c5dc
yeshwanth9724 <yeshwanth9724@gmail.com>
2026-08-06 10:44:21 +0530

    Add worst-fit memory allocation program

---
+++ Course-Practicals/Exercise-21.c
@@ -0,0 +1,58 @@
+#include <stdio.h>
+
+int main()
+{
+    int block[20],process[20],alloc[20];
+    int m,n;
+
+    printf("Enter number of blocks: ");
+    scanf("%d",&m);
+
+    printf("Enter block sizes:\n");
+    for(int i=0;i<m;i++)
+        scanf("%d",&block[i]);
+
+    printf("Enter number of processes: ");
+    scanf("%d",&n);
+
+    printf("Enter process sizes:\n");
+    for(int i=0;i<n;i++)
+        scanf("%d",&process[i]);
+
+    for(int i=0;i<n;i++)
+        alloc[i]=-1;
+
+    for(int i=0;i<n;i++)
+    {
+        int worst=-1;
+
+        for(int j=0;j<m;j++)
+        {
+            if(block[j]>=process[i])
+            {
+                if(worst==-1 || block[j]>block[worst])
+                    worst=j;
+            }
+        }
+
+        if(worst!=-1)
+        {
+            alloc[i]=worst;
+            block[worst]-=process[i];
+        }
+    }
+
+    printf("\nProcess\tSize\tBlock\n");
+
+    for(int i=0;i<n;i++)
+    {
+        printf("%d\t%d\t",i+1,process[i]);
+
+        if(alloc[i]!=-1)
+            printf("%d\n",alloc[i]+1);
+        else
+            printf("Not Allocated\n");
+    }
+
+    return 0;
+}
